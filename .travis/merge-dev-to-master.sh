94e594baf50de3afeef624ed12b3d404b7c6770a
Alexey Zaytsev <lonelylockley@gmail.com>
2019-08-18 11:36:15 +0300

    Debugging merge script

--- .travis/merge-dev-to-master.sh
+++ .travis/merge-dev-to-master.sh
@@ -12,20 +12,24 @@ echo "$FUNCTION_NAME: TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 
 if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ];
 then
-    echo "$FUNCTION_NAME: Exiting! Branch is not $SOURCE_BRANCH: ($TRAVIS_BRANCH)"
+	echo "$FUNCTION_NAME: Exiting! Branch is not $SOURCE_BRANCH: ($TRAVIS_BRANCH)"
     exit 0;
 fi
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
-    echo "$FUNCTION_NAME: Exiting! This is a Pull Request: $TRAVIS_PULL_REQUEST"
+	echo "$FUNCTION_NAME: Exiting! This is a Pull Request: $TRAVIS_PULL_REQUEST"
     exit 0;
 fi
 
-: ${GITHUB_SECRET_TOKEN:?"GITHUB_SECRET_TOKEN needs to be set in .travis.yml!"}
+echo "Debug"
+echo $GITHUB_SECRET_TOKEN
+echo $TRAVIS_COMMIT
+echo $SONATYPE_USERNAME
+echo "End debug"
 
-export GIT_COMMITTER_EMAIL="travis@travis.org"
-export GIT_COMMITTER_NAME="Travis CI"
+: ${GITHUB_SECRET_TOKEN:?"GITHUB_SECRET_TOKEN needs to be set in .travis.yml!"}
+: ${TRAVIS_COMMIT:?"TRAVIS_COMMIT needs to be available to merge the right commit to master!"}
 
 TEMPORARY_REPOSITORY=$(mktemp -d)
 git clone "https://github.com/$GITHUB_REPO" "$TEMPORARY_REPOSITORY"
@@ -33,12 +37,13 @@ cd $TEMPORARY_REPOSITORY
 
 echo "Checking out $SOURCE_BRANCH"
 git checkout $SOURCE_BRANCH
+git checkout -b tomerge $TRAVIS_COMMIT
 
 echo "Checking out $MERGE_BRANCH"
 git checkout $MERGE_BRANCH
 
-echo "Merging $SOURCE_BRANCH into $MERGE_BRANCH"
-git merge --ff-only "$SOURCE_BRANCH"
+echo "Merging temporary branch tomerge ($TRAVIS_COMMIT) from $SOURCE_BRANCH into $MERGE_BRANCH"
+git merge --ff-only "tomerge"
 
 echo "Pushing to $GITHUB_REPO"
 # Redirect to /dev/null to avoid secret leakage
